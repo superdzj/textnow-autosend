5fd141dd325c74eb7f33b0d21e72e10337f8a799
superdzj <superdzj@hotmail.com>
2021-02-08 19:24:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   username: process.env.TEXTNOW_USERNAME || "",
   password: process.env.TEXTNOW_PASSWORD || "",
-  recipient: process.env.TEXTNOW_RECIPIENT || "(726) 666-0002",
-  message: process.env.TEXTNOW_MESSAGE || "autosend message",
+  recipient: process.env.TEXTNOW_RECIPIENT || "",
+  message: process.env.TEXTNOW_MESSAGE || "",
   cookies_str: process.env.TEXTNOW_COOKIES || ""
 };
